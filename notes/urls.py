c85d39145bbf073340e2be2c2f3577494b1042f7
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-02-03 22:19:16 +0100

    feat: add responsive layout

--- notes/urls.py
+++ notes/urls.py
@@ -6,7 +6,6 @@ app_name = 'notes'
 
 urlpatterns = [
     path('', views.note_list, name='note_list'),
-    path('create/', views.note_create, name='note_create'),
     path('directories/', views.directory_list, name='directory_list'),
     path('ajax_update_note_order/', views.ajax_update_note_order, name='ajax_update_note_order'),
     path('update/<int:note_id>/', views.ajax_update_note, name='ajax_update_note'),
--- notes/views.py
+++ notes/views.py
@@ -127,45 +127,6 @@ def note_detail(request, slug):
                                                       })
 
 
-def note_create(request):
-    """
-    Show a basic HTML form (GET); handle creation (POST).
-    """
-    if request.method == 'POST':
-        title = request.POST.get('title', '').strip()
-        slug = request.POST.get('slug', '').strip()
-        content = request.POST.get('content', '')
-        index = request.POST.get('index', '0')
-        directory_id = request.POST.get('directory_id', '')
-
-        # Attempt converting index to int
-        try:
-            index = int(index)
-        except ValueError:
-            index = 0
-
-        directory = None
-        if directory_id:
-            # If a directory was chosen, fetch it
-            directory = get_object_or_404(Directory, pk=directory_id)
-
-        # If no slug is provided, it will be generated in .save()
-        new_note = Note(
-            title=title,
-            slug=slug,
-            content=content,
-            index=index,
-            directory=directory
-        )
-        new_note.save()  # Slug might be auto-generated
-
-        return redirect('notes:note_detail', slug=new_note.slug)
-
-    # GET case: show a form
-    directories = Directory.objects.all().order_by('index', 'title')
-    return render(request, 'notes/note_form.html', {'directories': directories})
-
-
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
