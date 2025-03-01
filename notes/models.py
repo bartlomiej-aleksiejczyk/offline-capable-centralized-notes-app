79043b6823971ed85f6f3f4fcf543908bb4cfd7d
Bartłomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2025-03-01 09:28:25 +0100

    feat: add note type to django notes app

--- notes/models.py
+++ notes/models.py
@@ -8,12 +8,13 @@ class Directory(models.Model):
     """
     A simple Directory model (not nested).
     """
+
     title = models.CharField(max_length=200, unique=True)
     index = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='directories'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="directories"
     )  # Use settings.AUTH_USER_MODEL for the custom user model
 
     def __str__(self):
@@ -24,21 +25,31 @@ class Note(models.Model):
     """
     A Note with an optional Directory parent.
     """
+
+    NOTE_TYPE_CHOICES = {
+        "PLAINTEXT": "Plaintext note",
+        "MARKDOWN": "Markdown note",
+        "TODO": "Todo list",
+    }
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     content = models.TextField()
     index = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    type = models.CharField(
+        max_length=255, choices=NOTE_TYPE_CHOICES, default="PLAINTEXT"
+    )
     directory = models.ForeignKey(
         Directory,
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='notes'
+        related_name="notes",
     )
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notes'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notes"
     )
 
     def save(self, *args, **kwargs):
@@ -51,4 +62,4 @@ class Note(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('notes:note_detail', kwargs={'slug': self.slug})
+        return reverse("notes:note_detail", kwargs={"slug": self.slug})
